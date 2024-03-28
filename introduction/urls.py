9a74e8ebcbde6d89938b246feecc3cc99ea4aabd
Jiyun <kjy5952@naver.com>
2024-03-29 01:12:40 +0900

    #91 Feat: add generate_presigned_url

--- introduction/urls.py
+++ introduction/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import image_view
+from .views import image_view, ImageUploadView
 
 urlpatterns = [
     path('photos/<str:image_name>/', image_view, name='image_view'),
+    path('generate_presigned_url/', ImageUploadView.as_view({'post': 'generate_presigned_url'}), name='generate_presigned_url'),
 ]
--- introduction/views.py
+++ introduction/views.py
@@ -2,26 +2,78 @@ from django.shortcuts import render
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 from rest_framework import viewsets, status
-# from .models import IntroImageUpload
-# from .serializers import IntroImageSerializer
+
+from kahluaproject.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+import boto3
 
-# class IntroImageView(viewsets.ModelViewSet):
-#     permission_classes = (IsAuthenticated,)
 
 def image_view(request, image_name):
     storage = S3Boto3Storage()
     file = storage.open(image_name)
-    # serializer = self.IntroImageSerializer(data=request.data)
-    # serializer.is_valid(raise_exception=True)
-    # data = serializer.validated_data
-
-    # image_location = settings.MEDIA_URL + data.image.title
-
+    
     return Response({
             'data': file
         },status=status.HTTP_200_OK)
+
+
+class ImageUploadView(viewsets.ModelViewSet):
+    permission_classes = (AllowAny, )
+
+    @swagger_auto_schema(
+        operation_id='presigned url 제공',
+        operation_description='''
+            s3에서 받아온 presigned url을 알려줍니다.
+        ''',
+        request_body=openapi.Schema(
+            'presigned url 제공',
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'file_name': openapi.Schema('file_name', type=openapi.TYPE_STRING),
+            }
+        ),
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        "status": "success",
+                        "data": "https://kahlua-bucket.s3/"
+                    }
+                }
+            )
+        }
+    )
+    def generate_presigned_url(self, request):
+        if request.method == 'POST':
+            file_name = request.POST.get('file_name')
+
+            if file_name:
+                # AWS S3 클라이언트 생성
+                s3_client = boto3.client('s3',
+                                        aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+                                
+                try:
+                    presigned_url = s3_client.generate_presigned_url('put_object',
+                                                                     Params={'Bucket': AWS_STORAGE_BUCKET_NAME,
+                                                                             'Key': file_name},
+                                                                     ExpiresIn=3600)  # url 만료시간
+                except Exception as e:
+                    return Response({
+                        'error': str(e),
+                    },status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                
+                return Response({
+                    'status': 'success',
+                    'presigned_url': presigned_url
+                }, status=status.HTTP_200_OK)
+            
+        return Response({
+            'error':'Invalid request',
+        }, status=status.HTTP_400_BAD_REQUEST)
